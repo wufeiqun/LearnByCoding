7db5fc59b32bfd9c38c26bff4417090f6b36e333
rocky <rocky@workubuntu.com>
2015-06-09 15:47:46 +0800

    get local ip

--- pyscript/get_local_ip.py
+++ pyscript/get_local_ip.py
@@ -10,7 +10,15 @@ def get_local_ip(ethname):
 	#返回的是1.1.1.1类似格式的IP地址.
 	#socket.inet_ntoa 能把32位的ipv4二进制地址转换为1.1.1.1类似的格式.
 	#s.fileno()返回socket的文件描述符,一个整数.
-	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0X8915, struct.pack('256s', ethname[:15]))[20:24]) 
+	print socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0X8915, struct.pack('256s', ethname[:15]))[20:24]) 
+
+def get_local_ipaddr():
+	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	s.connect_ex(('114.114.114.114',53))
+	(addr,port) = s.getsockname()
+	print addr
+	s.close()
 
 if __name__=='__main__': 
-	print get_local_ip('eth0')
+	get_local_ip('eth0')
+	get_local_ipaddr()
