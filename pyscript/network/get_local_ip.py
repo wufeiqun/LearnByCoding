4b2d3b3e8245e87d0759280e0d128e5958176aaf
rocky <rocky@workubuntu.com>
2015-06-09 16:08:53 +0800

    network

---
+++ pyscript/network/get_local_ip.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding=utf-8 
+import socket 
+import struct 
+import fcntl 
+
+def get_local_ip(ethname):
+	#定义一个socket对象,这里定义的是UDP方式. 
+	s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	#返回的是1.1.1.1类似格式的IP地址.
+	#socket.inet_ntoa 能把32位的ipv4二进制地址转换为1.1.1.1类似的格式.
+	#s.fileno()返回socket的文件描述符,一个整数.
+	print socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0X8915, struct.pack('256s', ethname[:15]))[20:24]) 
+
+def get_local_ipaddr():
+	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	s.connect_ex(('114.114.114.114',53))
+	(addr,port) = s.getsockname()
+	print addr
+	s.close()
+
+if __name__=='__main__': 
+	get_local_ip('eth0')
+	get_local_ipaddr()
