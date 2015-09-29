d369f67a2f442e0a7d9f54fe1f71da8952560718
rocky <wufeiqun@qfpay.com>
2015-09-29 17:50:44 +0800

    add some feature

--- pyscript/port_scan.py
+++ pyscript/port_scan.py
@@ -2,13 +2,31 @@
 import socket
 import sys
 
-def scan(port):
+def usage():
+	print '''
+1.列出某个主机的所有开放端口
+python port_scan.py ipaddr
+2.查看某个主机的某个端口是否打开
+python port_scan.py ipaddr port
+'''
+
+def scan(host,port):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.settimeout(0.1)
-    if s.connect_ex((sys.argv[1],port)) == 0:
-        print port
+    if s.connect_ex((host,int(port))) == 0:
+        print port,'is open'
 
     s.close()
 
 if __name__ == '__main__':
-   map(scan,xrange(10000))
+    if len(sys.argv) == 3:
+        host = sys.argv[1]
+        port = sys.argv[2]
+        scan(host,port)
+        sys.exit()
+    elif len(sys.argv) == 1:
+        host = '127.0.0.1'
+    elif len(sys.argv) == 2:
+        host = sys.argv[1]
+    for port in xrange(1,65536):
+        scan(host,port)
