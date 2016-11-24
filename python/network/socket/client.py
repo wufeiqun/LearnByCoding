b12686c4f0565b104aa4d83193113dfacaef4194
RockyWu <hellorocky728@gmail.com>
2016-11-24 12:24:34 +0800

    bug fix

--- python/network/socket/client.py
+++ python/network/socket/client.py
@@ -43,5 +43,5 @@ def main():
 
 
 if __name__ == "__main__":
-    single_client()
-    #main()
+    #single_client()
+    main()
--- python/network/socket/multi_thread_server.py
+++ python/network/socket/multi_thread_server.py
@@ -7,30 +7,35 @@ import socket
 import select
 import threading
 
-Thread_NUM = 0
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_addr = ("0.0.0.0", 8888)
-server.bind(server_addr)
-server.listen(5)
-print "Started server at %s:%s..." % server_addr
+class Server:
+    def __init__(self):
+        self.thread_num = 0
+        self.server_addr = ("127.0.0.1", 8888)
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(self.server_addr)
+        self.server.listen(5)
+        print "Started server at {0}:{1}...".format(*self.server_addr)
 
-def request(conn, addr):
-    while 1:
-        data = conn.recv(2048)
-        if not data:
-            break
-        print "Received data: {0} from {1}".format(data, addr)
-        conn.sendall(data)
-        print "Echo data: {0}".format(data)
-    conn.close()
+    def request(self, conn, addr):
+        while 1:
+            data = conn.recv(2048)
+            if not data:
+                break
+            print "Received data: {0} from {1}".format(data, addr)
+            conn.sendall(data)
+            print "Echo data: {0}".format(data)
+        conn.close()
 
-while True:
-    readable, writable, exceptional = select.select([server], [], [])
-    if server in readable:
-        newsocket, clientaddr = server.accept()
-        Thread_NUM += 1
-        t = threading.Thread(target=request, name="Server thread {0}".format(Thread_NUM), args=(newsocket, clientaddr,))
-        t.start()
-        print "Server thread {} has started...".format(Thread_NUM)
+    def start(self):
+        while True:
+            newsocket, clientaddr = self.server.accept()
+            self.thread_num += 1
+            t = threading.Thread(target=self.request, name="Server thread {0}".format(self.thread_num), args=(newsocket, clientaddr,))
+            t.start()
+            print "Server thread {} has started...".format(self.thread_num)
+
+if __name__ == "__main__":
+    s = Server()
+    s.start()
