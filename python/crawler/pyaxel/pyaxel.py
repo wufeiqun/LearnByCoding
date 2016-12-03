631d15fc51e11ab59cc145f53f982e0f0ac352e4
RockyWu <hellorocky728@gmail.com>
2016-12-03 18:34:42 +0800

    more

--- python/crawler/pyaxel/pyaxel.py
+++ python/crawler/pyaxel/pyaxel.py
@@ -15,6 +15,7 @@ import sys
 import glob
 import time
 import hashlib
+import tempfile
 import traceback
 import threading
 import contextlib
@@ -52,17 +53,16 @@ class Downloader:
         self.filename   = self.url.split("/")[-1]
         # Where to save the downloaded file, default is your current dir.
         self.put_dir    = "."
-        # Where to save the partial files temporary
-        if "download" not in os.listdir("/tmp"):
-            os.mkdir("/tmp/download")
-        self.tmp_dir    = "/tmp/download"
+        # Place to save the  temporary partial files
+        self.tmp_dir    = tempfile.TemporaryDirectory()
         self.filesize   = self.get_filesize()
         self.alloc      = self.filesize // self.thread_num
 
     def get_filesize(self):
         """Get content-length from headers (byte)"""
-        with contextlib.closing(urllib.request.urlopen(self.url)) as fp:
-            filesize = fp.length
+        req = urllib.request.Request(self.url, method="HEAD")
+        with contextlib.closing(urllib.request.urlopen(req)) as resp:
+            filesize = resp.length
             if not filesize:
                 raise KeyError("Content-Length not found from headers")
         return filesize
@@ -75,6 +75,7 @@ class Downloader:
         filename = threading.current_thread().name.split("-")[1]
 
         req = urllib.request.Request(self.url, headers=headers)
+
         with contextlib.closing(urllib.request.urlopen(req)) as resp:
             with open(os.path.join(self.tmp_dir, filename), "wb") as f:
                 while 1:
@@ -84,22 +85,31 @@ class Downloader:
                     f.write(block)
 
     def merge(self):
-        """Merge all the files orderly"""
-        # if filename exists,add suffix ".new"
+        """Merge all the files orderly and checksum the data"""
+        # if filename exists then append suffix with nums like "filename.0", "filename.1" etc.
         if os.path.exists(os.path.join(self.put_dir, self.filename)):
-            self.filename = self.filename + ".new"
-        new_file = open(os.path.join(self.put_dir, self.filename), "wb")
-        # Order by thread id
+            num_files = glob.glob(os.path.join(self.put_dir, self.filename + ".*"))
+            if num_files:
+                max_num = max([int(num_file.split(".")[-1]) for num_file in num_files])
+                self.filename = self.filename + str(max_num + 1)
+            else:
+                self.filename = self.filename + ".1"
+
+        merged_file = open(os.path.join(self.put_dir, self.filename), "wb")
+
+        # Order partial file by thread id
         sorted_flist = sorted(os.listdir(self.tmp_dir), key=lambda x: int(x))
+        # Merged the partial file
         for partial in sorted_flist:
             with open(os.path.join(self.tmp_dir, partial), "br") as f:
-                r = f.read(1024)
-                while r:
-                    new_file.write(r)
-                    r = f.read(1024)
-        new_file.close()
-        for dirty_file in glob.glob("/tmp/download/*"):
-            os.remove(dirty_file)
+                while 1:
+                    block = f.read(1024*8)
+                    if not block:
+                        break
+                    merged_file.write(block)
+        merged_file.close()
+        # Clean the tmpdir
+        self.tmp_dir.cleanup()
 
     def md5(self, filename):
         hash_md5 = hashlib.md5()
@@ -107,6 +117,15 @@ class Downloader:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
+
+    def info(self):
+        """
+        1. real URL
+        2. filesize
+        3. filename
+        4. file MD5
+        5. time consumtion
+        """
 
     def run(self):
         threads = []
