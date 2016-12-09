f79011dab6e871f79109ab6cfec11d7bcf274a1c
RockyWu <hellorocky728@gmail.com>
2016-12-09 18:51:41 +0800

    more

--- python/network/port_scan/gevt_pool.py
+++ python/network/port_scan/gevt_pool.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# coding:utf-8
 from gevent import monkey
 monkey.patch_all()
 import socket
@@ -9,7 +8,7 @@ def scan(port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.5)
     if client.connect_ex(("118.193.81.214", port)) == 0:
-        print "Port: {0} is open".format(port)
+        print("Port: {0} is open".format(port))
     client.close()
 
 if __name__ == "__main__":
