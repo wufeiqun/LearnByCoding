e8757a36c4c49583cbf2e258f210bc358c70eb28
RockyWu <hellorocky728@gmail.com>
2016-12-09 19:01:20 +0800

    more

--- python/network/port_scan/gevt_pool.py
+++ python/network/port_scan/gevt_pool.py
@@ -4,14 +4,20 @@ monkey.patch_all()
 import socket
 from gevent.pool import Pool
 
-def scan(port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.5)
-    if client.connect_ex(("118.193.81.214", port)) == 0:
-        print("Port: {0} is open".format(port))
-    client.close()
+class Scanner:
+    def __init__(self, host, timeout=0.5):
+        self.host = host
+        self.timeout = timeout
+
+    def scan(port):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(self.timeout)
+        if client.connect_ex((self.host, port)) == 0:
+            print("Port: {0} is open".format(port))
+        client.close()
 
 if __name__ == "__main__":
-    pool = Pool(500)
-    pool.map(scan, xrange(1, 65536))
+    pool = Pool(50)
+    s = Scanner("121.42.185.92")
+    pool.map(s.scan, range(1, 65536))
     pool.join()
