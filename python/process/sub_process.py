fa7bceb27430458912755e7099560169cf6556f6
RockyWu <hellorocky728@gmail.com>
2017-10-10 12:47:35 +0800

    subprocess

---
+++ python/process/sub_process.py
@@ -0,0 +1,20 @@
+import traceback
+import subprocess
+
+def run_cmd(cmd):
+    try:
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+        p.wait(timeout=10)
+        out = p.stdout.read() #type: str
+        err = p.stderr.read() #type: str
+        return p.returncode, out, err
+    except subprocess.TimeoutExpired:
+        print("执行代码超时: {0}".format(cmd))
+    except:
+        print(traceback.print_exc())
+    finally:
+        p.kill() #The child process is not killed if the timeout expires
+
+if __name__ == "__main__":
+    cmd = "sleep 15"
+    run_cmd(cmd)
