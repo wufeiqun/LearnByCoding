69c90eaa80938fcb32622e25a9254cb6aacbe6d6
RockyWu <hellorocky728@gmail.com>
2017-10-17 16:11:37 +0800

    rename

--- python/network/socket/poll_server.py
+++ python/network/socket/poll_server.py
@@ -36,9 +36,9 @@ fd_to_socket = {server.fileno(): server}
 socket_to_fd = {server: server.fileno()}
 
 while True:
-    print("事件监听中...")
     events = poller.poll(TIMEOUT)
     for fd, flag in events:
+        print("事件监听中...")
         sock = fd_to_socket[fd]
         if flag in (select.POLLIN, select.POLLPRI):
             if sock is server: # 说明是有新的客户端请求进来
@@ -52,25 +52,24 @@ while True:
                 # 这里使用了队列, 保存要返回的数据, 如果只是学习poll使用的话可以去掉, 让每个socket返回相同数据即可
                 message_queues[connection] = queue.Queue()
             else: # 说明已经建立连接的socket有数传送过来据等待接收
-                while True:
-                    data = sock.recv(1024)
-                    if data:
-                        print("接收到来自客户端: {1}:{2}的数据: {0}".format(data.decode(encoding="utf-8", errors="ignore"),
-                            *sock.getpeername()))
-                        message_queues[sock].put("谢谢你".encode())
-                        # 修改该socket监控状态为读写
-                        poller.modify(sock, READ_WRITE)
-                    else: # 客户端断开连接
-                        print("客户端: {0}:{1} 断开连接!".format(*sock.getpeername()))
-                        # 取消该socket的注册信息
-                        poller.unregister(sock)
-                        # 删除该socket的消息队列
-                        del message_queues[sock]
-                        # 从fd字段中删除该socket
-                        del fd_to_socket[socket_to_fd[sock]]
-                        del socket_to_fd[sock]
-                        # 关闭该socket
-                        sock.close()
+                data = sock.recv(1024)
+                if data:
+                    print("接收到来自客户端: {1}:{2}的数据: {0}".format(data.decode(encoding="utf-8", errors="ignore"),
+                        *sock.getpeername()))
+                    message_queues[sock].put("谢谢你".encode())
+                    # 修改该socket监控状态为读写
+                    poller.modify(sock, READ_WRITE)
+                else: # 客户端断开连接
+                    print("客户端: {0}:{1} 断开连接!".format(*sock.getpeername()))
+                    # 取消该socket的注册信息
+                    poller.unregister(sock)
+                    # 删除该socket的消息队列
+                    del message_queues[sock]
+                    # 从fd字段中删除该socket
+                    del fd_to_socket[socket_to_fd[sock]]
+                    del socket_to_fd[sock]
+                    # 关闭该socket
+                    sock.close()
         elif flag == select.POLLOUT: # 说明该socket是可写状态, 一般只要没有在可读状态的时候一个socket都是出于可写状态的
             try:
                 next_msg = message_queues[sock].get_nowait() # 从消息队列里获取数据(前面填充到该队列里的数据)
