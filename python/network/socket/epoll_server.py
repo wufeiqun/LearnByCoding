fc78e72422df1720e782fb6c7e011e9eddf9a8ac
RockyWu <hellorocky728@gmail.com>
2017-10-18 10:42:40 +0800

    first version

--- python/network/socket/epoll_server.py
+++ python/network/socket/epoll_server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-#优点: 1. 相比较于select, 监控的fd没有数量限制
-#缺点: 1. 只支持Linux, MacOS和Windows不支持(Mac上测试有bug)
+#优点: 1. 相比较于poll, 不随着监控的fd数量的增长而变慢, 基于事件
 import os
 import queue
 import select
@@ -13,12 +12,12 @@ class TCPServer:
         self.pid = os.getpid()
         self.server_address = (host, port)
         self.server = self.create_server()
-        self.timeout = timeout  # 单位(毫秒)
+        self.timeout = timeout  # 单位(秒)
         self.message_queues = {}
-        self.READ_ONLY = (select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR)
-        self.READ_WRITE = (self.READ_ONLY | select.POLLOUT)
-        self.poller = select.poll()
-        self.poller.register(self.server.fileno(), self.READ_ONLY)
+        self.READ_ONLY = (select.EPOLLIN | select.EPOLLPRI | select.EPOLLHUP | select.EPOLLERR)
+        self.READ_WRITE = (self.READ_ONLY | select.EPOLLOUT)
+        self.epoller = select.epoll()
+        self.epoller.register(self.server.fileno(), self.READ_ONLY)
         # 由于poll返回的是一个数组, 数组中的元素为元组, 元组格式为(fd, flag), 所以有必要定义一个fd_to_socket
         self.fd_to_socket = {self.server.fileno(): self.server}
         # 由于客户端socket关闭以后要删除fd_to_socket中保存的socket, 但是socket是value, 为了更加快速删除该条记录
@@ -40,7 +39,7 @@ class TCPServer:
             self.fd_to_socket[connection.fileno()] = connection
             self.socket_to_fd[connection] = connection.fileno()
             # 将新来的客户端socket注册到监听列表中
-            self.poller.register(connection, self.READ_ONLY)
+            self.epoller.register(connection.fileno(), self.READ_ONLY)
             # 这里使用了队列, 保存要返回的数据, 如果只是学习poll使用的话可以去掉, 让每个socket返回相同数据即可
             self.message_queues[connection] = queue.Queue()
         else:  # 说明已经建立连接的socket有数传送过来据等待接收
@@ -50,11 +49,11 @@ class TCPServer:
                                                          *sock.getpeername()))
                 self.message_queues[sock].put("谢谢你\n".encode())
                 # 修改该socket监控状态为读写
-                self.poller.modify(sock, self.READ_WRITE)
+                self.epoller.modify(sock, self.READ_WRITE)
             else:  # 客户端断开连接
                 print("客户端: {0}:{1} 断开连接!".format(*sock.getpeername()))
                 # 取消该socket的注册信息
-                self.poller.unregister(sock)
+                self.epoller.unregister(sock)
                 # 删除该socket的消息队列
                 del self.message_queues[sock]
                 # 从fd字段中删除该socket
@@ -67,14 +66,14 @@ class TCPServer:
         try:
             next_msg = self.message_queues[sock].get_nowait()  # 从消息队列里获取数据(前面填充到该队列里的数据)
         except queue.Empty:  # 该socket的消息队列为空的时候表示已经读取并回复了, 这时候应该只监听该socket是否可读即可
-            self.poller.modify(sock, self.READ_ONLY)
+            self.epoller.modify(sock, self.READ_ONLY)
         else:
             sock.send(next_msg)
 
     def handle_exception(self, sock):
         print("连接: {0}:{1} 发生异常!".format(*sock.getpeername()))
         # 取消该socket的注册信息
-        self.poller.unregister(sock)
+        self.epoller.unregister(sock)
         # 删除该socket的消息队列
         del self.message_queues[sock]
         # 从fd字段中删除该socket
@@ -85,25 +84,25 @@ class TCPServer:
     def run(self):
         print("Started server at {0}:{1}, PID: {2}".format(*self.server_address, self.pid))
         while True:
-            events = self.poller.poll(self.timeout)
+            events = self.epoller.poll(self.timeout)
             if not events:
                 print("继续事件监听中...")
                 continue
             for fd, event in events:
                 sock = self.fd_to_socket[fd]
-                if event in (select.POLLIN, select.POLLPRI):
+                if event == select.EPOLLIN:
                     self.handle_receive(sock)
-                elif event == select.POLLOUT: # 说明该socket是可写状态, 一般只要没有在可读状态的时候一个socket都是出于可写状态的
+                elif event == select.EPOLLOUT:  # 说明该socket是可写状态, 一般只要没有在可读状态的时候一个socket都是出于可写状态的
                     self.handle_send(sock)
-                elif event in (select.POLLERR, select.POLLHUP):
+                elif event in (select.EPOLLERR, select.EPOLLHUP):
                     self.handle_exception(sock)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="简单的TCP回显服务器!")
+    parser = argparse.ArgumentParser(description="简单的TCPServer!")
     parser.add_argument("--hostname", dest="hostname", default="0.0.0.0", metavar="IP", help="请输入监听的IP地址")
     parser.add_argument("--port", dest="port", type=int, default=8888, metavar="端口", help="请输入监听的端口")
-    parser.add_argument("--timeout", dest="timeout", type=int, default=5000, metavar="超时时间(毫秒)", help="请输入超时时间")
+    parser.add_argument("--timeout", dest="timeout", type=int, default=5, metavar="超时时间(秒)", help="请输入超时时间")
     args = parser.parse_args()
     server = TCPServer(args.hostname, args.port, args.timeout)
     server.run()
