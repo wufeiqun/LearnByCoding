9a2d5f9f0ae5fd620eb5019188d10f9957467828
RockyWu <hellorocky728@gmail.com>
2017-11-16 13:55:15 +0800

    golang

---
+++ golang/network/server.go
@@ -0,0 +1,45 @@
+//TCPServer
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+	host := "0.0.0.0"
+	port := flag.String("port", "8888", "监听端口") //指针类型, 通过*port来获取真实的值
+
+	flag.Parse()
+
+	listener, err := net.Listen("tcp4", host+":"+*port)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Println("Server start at: " + host + ":" + *port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err)
+			continue
+		}
+
+		go handle(conn)
+
+	}
+
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	fmt.Printf("Received new connection: %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
+	for {
+		io.Copy(conn, conn)
+	}
+}
