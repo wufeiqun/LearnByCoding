1b37a142e4876fee2cf3a2b08f21c1a1fc0d241f
RockyWu <hellorocky728@gmail.com>
2018-01-11 15:54:57 +0800

    find

--- python/other/random_strings.py
+++ python/other/random_strings.py
@@ -5,4 +5,4 @@ def random_string(length=8):
     return "".join(random.choices(string.ascii_letters+string.digits, k=length))
 
 if __name__ == "__main__":
-    print(random_string())
+    print(random_string(10))
